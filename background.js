f917811eb0d78e34075f4f6f2c8bd9a123158ecb
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2021-10-29 08:18:28 +0200

    Create background.js

---
+++ background.js
@@ -0,0 +1,36 @@
+"use strict";
+chrome.runtime.onInstalled.addListener((function(){
+  chrome.tabs.query({
+    url:[
+      "https://www.youtube.com/*",
+      "https://music.youtube.com/*",
+      "https://m.youtube.com/*"
+    ]
+  },
+  t => {
+    for(let o of t){
+      console.log(o);
+      chrome.tabs.reload(o.id)
+    }
+  })
+}));
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'www.youtube.com' }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'music.youtube.com' }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'm.youtube.com' }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+});
