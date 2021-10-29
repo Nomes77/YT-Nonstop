2175c3237384ec7c041346653ebf8febd393d3c2
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2021-10-29 14:32:03 +0200

    Add files via upload

--- autoconfirm.js
+++ autoconfirm.js
@@ -1,24 +1,6 @@
-var ynsInjection =
-  '(' +
-  function () {
-    const tag = '[YT-Nonstop]';
-    const isYoutubeMusic = window.location.hostname === 'music.youtube.com';
-
-    function log(message) {
-      console.log(`${tag}[${getMilliseconds()}] ${message}`);
-    }
-
-    function getMilliseconds() {
-      return +new Date();
-    }
-
-    setInterval(() => window._lact = Date.now(), 900000);
-      log('Active Time Reset');
-  } +
-  ')();';
-console.log(`[YT-Nonstop ${chrome.runtime.getManifest().version}]`);
-
-var script = document.createElement('script');
-script.textContent = ynsInjection;
+const script = document.createElement('script');
+script.textContent = 'setInterval(() => window._lact = Date.now(), 900000);';
+script.onload = function () {
+    this.remove();
+};
 (document.head || document.documentElement).appendChild(script);
-script.remove();
