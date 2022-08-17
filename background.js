97730774c55b2cae25eec0ea20fa9894d24f65ba
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2022-08-17 12:04:34 +0000

    Update background.js

--- background.js
+++ background.js
@@ -14,3 +14,24 @@ chrome.runtime.onInstalled.addListener((function(){
     }
   })
 }));
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.disable();
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    let activeWebsites = {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'www.youtube.com' }
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'music.youtube.com' }
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'm.youtube.com' }
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowAction()]
+    };
+    let rules = [activeWebsites];
+    chrome.declarativeContent.onPageChanged.addRules(rules);
+  });
+});
