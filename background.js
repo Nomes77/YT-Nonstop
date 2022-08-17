f47e438bd50c4cd97a8cab9f7cfd95f22481ff48
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2022-08-17 09:51:25 +0000

    only for Microsoft Edge

--- background.js
+++ background.js
@@ -14,3 +14,23 @@ chrome.runtime.onInstalled.addListener((function(){
     }
   })
 }));
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'www.youtube.com' }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'music.youtube.com' }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'm.youtube.com' }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()]
+      }
+    ]);
+  });
+});
