dbd730481642afcc6a160d99245554c8a216541b
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2022-08-19 14:47:53 +0000

    updated

--- autoconfirm.js
+++ autoconfirm.js
@@ -1,28 +1,130 @@
-// Variables
-let target = ["YT-CONFIRM-DIALOG-RENDERER"];
+var ytnsInjection =
+  '(' +
+  function () {
+    const YTMusic = window.location.hostname === 'music.youtube.com';
+    const appName = YTMusic ? 'ytmusic-app' : 'ytd-app';
+    const popupEventNodename = YTMusic ? 'YTMUSIC-YOU-THERE-RENDERER' : 'YT-CONFIRM-DIALOG-RENDERER';
+    const popupContainer = YTMusic ? 'ytmusic-popup-container' : 'ytd-popup-container';
 
-// custom quick functions
-function close(b){
-  b.click();
-}
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    let appObserver = null;
 
-// 1. Method: Set last time active all 10 minutes to now | Does probably not help
-setInterval(() => window._lact = Date.now(), 600000);
+    let pauseRequested = false;
+    let pauseRequestedTimeout;
+    const TimeoutMillis = 5000;
+    let lastInteractionTime = new Date().getTime();
 
-// 2. Method: Look for popup which gets dynamicially created
-const settings = { childList: true, subtree: true };
-const documentTarget = document.documentElement;
-const callback = function(mutationsList, observer) {
-  for (const mutation of mutationsList) {
-    if (mutation.target.nodeName === target[0]) {
-      let a = document.getElementsByTagName("yt-confirm-dialog-renderer")[0];
-      let b = a.getElementsByClassName("style-scope yt-confirm-dialog-renderer style-blue-text size-default")[0];
-      close(b);
+    let videoElement = null;
+
+    function isIdle() {
+      return getIdleTime() >= TimeoutMillis;
     }
-  }
-}
-const observer = new MutationObserver(callback);
-observer.observe(documentTarget, settings);
+    function getIdleTime() {
+      return new Date().getTime() - lastInteractionTime;
+    }
 
-// 3. Method: If popup is already open close it
-if (document.getElementsByTagName("yt-confirm-dialog-renderer")[0].getElementsByTagName("yt-button-renderer")[1]) close(document.getElementsByTagName("yt-confirm-dialog-renderer")[0].getElementsByTagName("yt-button-renderer")[1]);
+    function listenForMediaKeys() {
+      if (navigator.mediaSession === undefined) {
+        return;
+      }
+      navigator.mediaSession.setActionHandler('pause', () => {
+        pauseVideo();
+      });
+      navigator.mediaSession.ytns_setActionHandler = navigator.mediaSession.setActionHandler;
+      navigator.mediaSession.setActionHandler = (action, fn) => {
+        if (action === 'pause') {
+          return;
+        }
+        navigator.mediaSession.ytns_setActionHandler(action, fn);
+      };
+    }
+
+    function listenForMouse() {
+      const eventName = window.PointerEvent ? 'pointer' : 'mouse';
+      document.addEventListener(eventName + 'down', (e) => {
+        processInteraction(eventName + 'down');
+      });
+      document.addEventListener(eventName + 'up', (e) => {
+        processInteraction(eventName + 'up');
+      });
+    }
+
+    function listenForKeyboard() {
+      document.addEventListener('keydown', (e) => {
+        processInteraction('keydown');
+      });
+      document.addEventListener('keyup', (e) => {
+        processInteraction('keyup');
+      });
+    }
+
+    function processInteraction(action) {
+      if (pauseRequested) {
+        pauseVideo();
+        return;
+      }
+      lastInteractionTime = new Date().getTime();
+    }
+
+    function observeApp() {
+      appObserver = new MutationObserver((mutations, observer) => {
+        overrideVideoPause();
+      });
+      appObserver.observe(document.querySelector(appName), {
+        childList: true,
+        subtree: true
+      });
+    }
+
+    function listenForPopupEvent() {
+      document.addEventListener('yt-popup-opened', (e) => {
+        if (isIdle() && e.detail.nodeName === popupEventNodename) {
+          document.querySelector(popupContainer).handleClosePopupAction_();
+          pauseVideo();
+          videoElement.play();
+        }
+      });
+    }
+
+    function overrideVideoPause() {
+      if (videoElement?.ytns_pause !== undefined) return;
+      if (document.querySelector('video') === null) return;
+      videoElement = document.querySelector('video');
+      listenForMediaKeys();
+      videoElement.ytns_pause = videoElement.pause;
+      videoElement.pause = () => {
+        if (!isIdle()) {
+          pauseVideo();
+          return;
+        }
+        pauseRequested = true;
+        setPauseRequestedTimeout();
+      };
+    }
+
+    function setPauseRequestedTimeout(justClear = false) {
+      clearTimeout(pauseRequestedTimeout);
+      if (justClear) return;
+      pauseRequestedTimeout = setTimeout(() => {
+        pauseRequested = false;
+      }, TimeoutMillis);
+    }
+
+    function pauseVideo() {
+      videoElement?.ytns_pause();
+      pauseRequested = false;
+      setPauseRequestedTimeout(true);
+    }
+
+    listenForMouse();
+    listenForKeyboard();
+
+    listenForPopupEvent();
+    observeApp();
+  } +
+  ')();';
+
+var script = document.createElement('script');
+script.textContent = ytnsInjection;
+(document.head || document.documentElement).appendChild(script);
+script.remove();
